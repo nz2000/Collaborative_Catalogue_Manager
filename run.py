40d1a42417dfe1a6dc3d9d1b35f8e12ff29c204b
Noel Zacharia <noelzacharia8@gmail.com>
2020-08-01 17:05:49 -0400

    Files have been updated

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+'''
+Author: Noel Zacharia
+Objective: To create a Collaborative Catalogue Manager for University students
+'''
+
+#This is the main code to run the website
 from Flask_Blog import app
 
 if __name__ == '__main__':
